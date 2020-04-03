871b0320beea28c3ae5a5203b36568decfccf1cb
René Vidríales Trujillo <vidrialestrujillorene@gmail.com>
2020-04-02 18:09:37 -0600

    WIP, added content grabbing and partially finished the initial screen AB#5, AB#3

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import Layout from './Layout';
 import SelectOrDragImage from '../components/SelectOrDragImage';
+import Link from '../components/Link';
 import '../styles/home.css';
+import { initialMessage } from '../media/strings.json';
 
 export default function Home() {
   return (
@@ -10,9 +12,16 @@ export default function Home() {
         <section className="SelectImages">
           <SelectOrDragImage />
         </section>
-        <article className="HomeArticle">
-          <h1>Acerca de este sitio</h1>
-        </article>
+        <section className="HomeSection">
+          <article className="HomeArticle">
+            <h1 style={{marginTop: '2%'}}>Acerca de este sitio</h1>
+            <p style={{width: '95%', marginTop: '3%', textAlign: 'justify'}}>{initialMessage}</p>
+            <section className="HomeButtons">
+              <Link styles='BaseButton HomeButton' to='' label='PREDECIR'/>
+              <Link styles='BaseButton HomeButton' to='' label='LIMPIAR'/>
+            </section>
+          </article>
+        </section>
       </div>
     </Layout>
   );
