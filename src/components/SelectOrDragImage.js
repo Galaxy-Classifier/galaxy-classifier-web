8516ac66f8fda6db1c829810a18a6e3f6e4ce6fb
René Vidríales Trujillo <15352027+renecvt@users.noreply.github.com>
2020-04-06 20:22:53 -0600

    Select image (#6)
    * Layout component (#4)
    
    * Added social tags and favicon generation for many platforms, closes #2
    
    * Changed a conflicting command, still closes #2
    
    * Updated job name, will close #2
    
    * Set checkout commits to be just for Windows, effectively closing #2
    
    * Closes #3 and addresses AB#33
    
    * Fixes eslint errors, closes #3
    
    * WIP #1, AB#33
    
    * Added basic styling, added proptypes, fixed React router implementation, closes #3, addresses #2 and AB#33
    
    * WIP #1, AB#33
    
    * Closes AB#33, closes #1, closes #3
    
    * Added events for drag and drop, finished gallery opener AB#5, almost finished AB#3
    
    * WIP, added content grabbing and partially finished the initial screen AB#5, AB#3
    
    * New dependency tree, changed state control to parent component, added image visualization, fixed some css classes, added animations, added modals without style AB#5, AB#10, AB#3, AB#7
    
    * Closes AB#3, closes AB#5, closes AB#7, closes AB#10
    
    * Fixed eslint errors and added props validation for new functions, needs to refactor the modal actions. AB#3, AB#5, AB#7, AB#10
    
    * Fixed line endings

---
+++ src/components/SelectOrDragImage.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const acceptedTypes = ['image/png', 'image/jpg', 'image/jpeg'];
+
+function preventDefaults(event) {
+  event.preventDefault();
+  event.stopPropagation();
+}
+
+function addHighlight(event) {
+  preventDefaults(event);
+  document.getElementById('Draggeable').classList.add('highlight');
+}
+
+function removeHighlight(event) {
+  preventDefaults(event);
+  document.getElementById('Draggeable').classList.remove('highlight');
+}
+
+function getDraggedImages(event, setSelectedImage, setNotCompliantImages, setImages) {
+  removeHighlight(event);
+  const { dataTransfer: { files } } = event;
+  const compliant = checkFilesCompliance(files);
+
+  if (compliant) {
+    setSelectedImage(true);
+    setImages(compliant);
+  } else {
+    setNotCompliantImages(true);
+  }
+}
+
+function getFileExplorerImages(event, setSelectedImage, setNotCompliantImages, setImages) {
+  const { target: { files } } = event;
+  const compliant = checkFilesCompliance(files);
+
+  if (compliant) {
+    setSelectedImage(true);
+    setImages(compliant);
+  } else {
+    setNotCompliantImages(true);
+  }
+}
+
+function checkFilesCompliance(files) {
+  const filesAsArray = Array.from(files).slice(0, 3);
+
+  if (filesAsArray.length < 3) {
+    return false;
+  }
+
+  const notCompliantElement = filesAsArray.map(item => item.type).find(type => !acceptedTypes.includes(type));
+  if (notCompliantElement) return false;
+
+  return filesAsArray;
+}
+
+const renderImages = (images) => images.map(image => (<img key={image.lastModified} className='SelectedImage' src={window.URL.createObjectURL(image)} />));
+
+export default function SelectOrDragImage(props) {
+  return (
+    <div
+      onDragEnter={(event) => addHighlight(event)}
+      onDragLeave={(event) => removeHighlight(event)}
+      onDragOver={(event) => addHighlight(event)}
+      onDrop={(event) => getDraggedImages(event, props.setSelectedImages, props.setNotCompliantImages, props.setImages)}
+      className="SelectedImageContainer"
+      id="Draggeable"
+    >
+      <form id="SelectImageForm" className="SelectImageForm">
+        <label>{props.containerMessage}</label>
+        <input 
+          onChange={(event) => getFileExplorerImages(event, props.setSelectedImages, props.setNotCompliantImages, props.setImages)} 
+          multiple 
+          type="file" 
+          accept="image/png, image/jpeg, image/jpg"
+        />
+      </form>
+      { props.images.length ? 
+        <div className="animated fadeIn SelectedImagesContainer">{renderImages(props.images)}</div> : 
+        null 
+      }
+    </div>
+  );
+}
+
+SelectOrDragImage.propTypes = {
+  shouldClearImages: PropTypes.bool,
+  resetClearImages: PropTypes.func,
+  fileManager: PropTypes.func,
+  setSelectedImages: PropTypes.func,
+  setNotCompliantImages: PropTypes.func,
+  setImages: PropTypes.func,
+  containerMessage: PropTypes.string,
+  images: PropTypes.array,
+};
