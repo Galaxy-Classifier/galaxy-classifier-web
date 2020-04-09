f0822be6c576ace73f59c93180fd34c986179334
René Vidríales Trujillo <15352027+renecvt@users.noreply.github.com>
2020-04-09 12:26:44 -0600

    Galaxies information page (#9) (#10)
    * Finished the AB#31 task, which means this page is finished
    
    * Closes AB#31, adds proptypes to the galaxy item component, needs CSS refactoring

---
+++ src/components/GalaxyItem.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Picture from './Picture';
+import '../styles/galaxies.css';
+
+export default function GalaxyItem(props) {
+  return (
+    <article className='GalaxyItem'>
+      <Picture pictureElementClasses='GalaxyPictureClass' classes='GalaxyImage' src={props.src} />
+      <article className='GalaxyItemArticle'>
+        <h1>{props.title}</h1>
+        <ul className='GalaxyItemList'>
+          {props.info.map((info) => <li key={info.id}><p>{info.text}</p></li>)}
+        </ul>
+      </article>
+    </article>
+  );
+}
+
+GalaxyItem.propTypes = {
+  src: PropTypes.string,
+  title: PropTypes.title,
+  info: PropTypes.array,
+};
